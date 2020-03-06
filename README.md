2c70d7e46722e0d0946bd40197b437d22577a8b0
Suikan <26223147+suikan4github@users.noreply.github.com>
2020-03-07 08:18:25 +0900

    Added d005 control.

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ A list of the known STM32 HAL defects
         - [D001 Nucleo F722ZE wrong clock configuration](#d001-nucleo-f722ze-wrong-clock-configuration)
         - [D002 STM32F7 HAL_EXTI_SetConfigLine() runtime bug](#d002-stm32f7-hal_exti_setconfigline-runtime-bug)
         - [D003 STM32G0 HAL_GPIO_EXTI_Callback() incompatibility](#d003-stm32g0-hal_gpio_exti_callback-incompatibility)
-        - [D004 STM32H7 HAL_I2C_Master_Transmit_IT() runtime bug](#d004-stm32h7-hal_i2c_master_transmit_it-runtime-bug)
+        - [D004 STM32H7 HAL_I2C_Master_Transmit_IT() NAK response is slow](#d004-stm32h7-hal_i2c_master_transmit_it-nak-response-is-slow)
         - [D005 STM32L1 HAL_I2C_Master_Sequential_Transmit_IT() incompatibility](#d005-stm32l1-hal_i2c_master_sequential_transmit_it-incompatibility)
         - [D006 STM32L1 HAL_EXTI wrong configuration](#d006-stm32l1-hal_exti-wrong-configuration)
     - [Resolved](#resolved)
@@ -17,6 +17,10 @@ A list of the known STM32 HAL defects
 
 <!-- /TOC -->
 ## Preface
+These are the correction of the STM32 series CubeIDE/HAL related software bugs. I found them during the development of the [murasaki library](https://github.com/suikan4github/murasaki). So, only murasaki related bugs are listed. 
+
+Pull requests are welcome but please send the one which is relevant to the [murasaki library](https://github.com/suikan4github/stm32-defects). Any other pull requests will be rejected. 
+
 Repository URL : https://github.com/suikan4github/stm32-defects
 
 ## Defects
@@ -37,7 +41,7 @@ This is a collection of the STM32 software bugs which is harmful to the developm
 #### Description
 In the CubeIDE output project, the default input clock frequency of the Nucleo F722ZE is wrong. It is configured as 25MHz, while it should be 8MHz. 
 
-#### How to reprodcue
+#### How to reproduce
 Create a new Nucleo F722ZE board project from CubeIDE. By default, the HSE clock frequency is set as 25MHz. 
 
 #### Consideration
@@ -70,7 +74,7 @@ The HAL_EXTI_SetConfigLine() generates interrupt infinitely, after setting the r
 #### Consideration
 At the initial state, only LED2( Blue )is turned on. And both LED2 and LED3 ( Blue and Red ) are toggled in the EXTI interrupt call back. Thus, we are watching EXTI13 interrupt is accepted continuously. This is not expected behavior. 
 
-In the main routine, saved EXTI 13 ( interrupt of the Switch B1 ) interrupt configuration, reset it, and then, re-store the EXTI 13 configuration. This must just enabe the egdge trigger interrupt. In case of correct implementation, this program goggles LED2 and LED3 only when B1 is pushed. 
+In the main routine, saved EXTI 13 ( interrupt of the Switch B1 ) interrupt configuration, reset it, and then, re-store the EXTI 13 configuration. This must just enable the edge trigger interrupt. In case of correct implementation, this program goggles LED2 and LED3 only when B1 is pushed. 
 
 ```
     HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_SET);
@@ -121,7 +125,7 @@ The HAL_GPIO_EXTI_Callback() is not called while EXTI interrupt is accepted.
  If the program runs correctly, the green LED brinks for each time the switch B1 is pushed. 
 
 #### Consideration
-For the STM32G0, ST changed call back specification as imcompatible manner with other series. Thus, the HAL_GPIO_EXTI_Callback() is not called anymore. This is bad way to implement a Hardware "Abstraction" Layer. 
+For the STM32G0, ST changed call back specification as incompatible manner with other series. Thus, the HAL_GPIO_EXTI_Callback() is not called anymore. This is bad way to implement a Hardware "Abstraction" Layer. 
 
 ```
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
@@ -139,7 +143,7 @@ The project "d003-nucleo-g431rb-control" shows the expected behavior. For each t
 
 Run this program on Nucleo G431RB.
 
-### D004 STM32H7 HAL_I2C_Master_Transmit_IT() runtime bug
+### D004 STM32H7 HAL_I2C_Master_Transmit_IT() NAK response is slow
 | Item                    | Description                |
 | ----------------------- | -------------------------- |
 | Affected device         | STM32H7                    |
@@ -152,11 +156,7 @@ Run this program on Nucleo G431RB.
 | Reported                | [ST Community](https://community.st.com/s/question/0D50X0000BoyRlfSQE/bug-report-h743-hali2ctransmitit-respond-incorrectly-when-it-receives-nak)               |
 
 #### Description
-According to HAL manner, when I2C NAK is received, the HAL_I2C_ErrorCallback() must be called immediately. But the HAL_I2C_MasterTxCpltCallback() is called several seconds after NAK received.
-
-This is wrong by two means. 
- 1. The response is not immediate
- 1. Wrong callback is called. 
+According to HAL manner, when I2C NAK is received, the HAL_I2C_ErrorCallback() must be called immediately. But it is called several seconds after NAK received.
 
 #### How to reproduce
  1. Clone this repository
@@ -164,20 +164,46 @@ This is wrong by two means.
  3. Build and run on Nucleo H743. Do not mount any I2C device.
 
  In this hardware setup, there is no I2C device. Thus, any I2C master access cause NAK response
-(No response is interpreted as NAK on the I2C bus). If this program run correctly, we will see the
-red LED turns on by HAL_I2C_ErrorCallback(). But we will see the blue LED turn on HAL_I2C_MasterTxCpltCallback()
-because of the bug in the HAL. And ther is huge delay. 
+(No response is interpreted as NAK on the I2C bus). If this program run correctly, we will see the red LED turns on by HAL_I2C_ErrorCallback() immediately. But we see the red LED turns on after several second later. This is problem. 
+
+Note that the target I2C device address of the demo program is 1. Connecting any I2C device except address == 1 shows same result. 
 
 #### Consideration
-This is the bug of the I2C interrupt handler inside HAL. 
+This is a bug of the I2C interrupt handler inside HAL. 
 
 #### Control program
 The project "d004-nucleo-g431rb-control" shows the expected behavior. Without any IO board, there is no I2C device. You will see the green LED is on immediately by HAL_I2C_ErrorCallback()
 
 Run this program on Nucleo G431RB.
 
-
 ### D005 STM32L1 HAL_I2C_Master_Sequential_Transmit_IT() incompatibility
+| Item                    | Description                |
+| ----------------------- | -------------------------- |
+| Affected device         | STM32L1                    |
+| Last reproduced CubeIDE | -                          | 
+| Resolved CubeIDE        | -                          |
+| Last reproduced FW      | L1 v1.9.0                  | 
+| Resolved FW             | -                          |
+| Demo program            | d005-nucleo-l152-i2c       |
+| Control program         | d005-nucleo-g431rb-control |
+| Reported                | [ST Community](https://community.st.com/s/question/0D50X0000BbMfEjSQK/stm32l1-hal-has-compatibility-problems-on-i2c-api)               |
+
+#### Description
+Calling HAL_I2C_Master_Sequential_Transmit_IT() causes build error. 
+
+#### How to reproduce
+ 1. Clone this repository
+ 2. Import the project "d002-nucleo-f746-exti" from git repository to the CubeIDE workspace.
+ 3. Build this project. 
+
+The HAL_I2C_Master_Sequential_Transmit_IT() is on the HAL manual. But causes compile error. 
+
+#### Consideration
+This is bug in the HAL. The expected functionality is given by HAL_I2C_Master_Seq_Transmit_IT(). Other STM32 series has compatibility macro to rename from HAL_I2C_Master_Seq_Transmit_IT() to HAL_I2C_Master_Sequential_Transmit_IT(), but ST forgot to provide for STM32L1 series. 
+
+#### Control program
+The project "d005-nucleo-g431rb-control" shows the expected behavior. Building this program shows no error. 
+
 ### D006 STM32L1 HAL_EXTI wrong configuration
 
 ## Resolved
--- d005-nucleo-g431rb-control/Core/Src/main.c
+++ d005-nucleo-g431rb-control/Core/Src/main.c
@@ -6,7 +6,7 @@
  ******************************************************************************
  * @attention
  *
- * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+ * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
  * All rights reserved.</center></h2>
  *
  * This software component is licensed by ST under BSD 3-Clause license,
@@ -33,7 +33,6 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -44,7 +43,7 @@
 /* Private variables ---------------------------------------------------------*/
 I2C_HandleTypeDef hi2c1;
 
-UART_HandleTypeDef huart2;
+UART_HandleTypeDef hlpuart1;
 
 /* USER CODE BEGIN PV */
 
@@ -53,7 +52,7 @@ UART_HandleTypeDef huart2;
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
-static void MX_USART2_UART_Init(void);
+static void MX_LPUART1_UART_Init(void);
 static void MX_I2C1_Init(void);
 /* USER CODE BEGIN PFP */
 
@@ -93,7 +92,7 @@ int main(void)
 
     /* Initialize all configured peripherals */
     MX_GPIO_Init();
-    MX_USART2_UART_Init();
+    MX_LPUART1_UART_Init();
     MX_I2C1_Init();
     /* USER CODE BEGIN 2 */
 #if 1
@@ -105,6 +104,7 @@ int main(void)
     // Perhaps, bug in the stm32_hal_legacy.h
     HAL_I2C_Master_Seq_Transmit_IT(&hi2c1, 0x38, txbuf, 5, 0);
 #endif
+
     /* USER CODE END 2 */
 
     /* Infinite loop */
@@ -126,10 +126,11 @@ void SystemClock_Config(void)
 {
     RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
     RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
+    RCC_PeriphCLKInitTypeDef PeriphClkInit = { 0 };
 
     /** Configure the main internal regulator output voltage
      */
-    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+    HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1_BOOST);
     /** Initializes the CPU, AHB and APB busses clocks
      */
     RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
@@ -137,8 +138,11 @@ void SystemClock_Config(void)
     RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
     RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
     RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL6;
-    RCC_OscInitStruct.PLL.PLLDIV = RCC_PLL_DIV3;
+    RCC_OscInitStruct.PLL.PLLM = RCC_PLLM_DIV4;
+    RCC_OscInitStruct.PLL.PLLN = 85;
+    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+    RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
+    RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
     if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
             {
         Error_Handler();
@@ -152,7 +156,16 @@ void SystemClock_Config(void)
     RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
     RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_8) != HAL_OK)
+            {
+        Error_Handler();
+    }
+    /** Initializes the peripherals clocks
+     */
+    PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_LPUART1 | RCC_PERIPHCLK_I2C1;
+    PeriphClkInit.Lpuart1ClockSelection = RCC_LPUART1CLKSOURCE_PCLK1;
+    PeriphClkInit.I2c1ClockSelection = RCC_I2C1CLKSOURCE_PCLK1;
+    if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
             {
         Error_Handler();
     }
@@ -174,15 +187,27 @@ static void MX_I2C1_Init(void)
 
     /* USER CODE END I2C1_Init 1 */
     hi2c1.Instance = I2C1;
-    hi2c1.Init.ClockSpeed = 100000;
-    hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+    hi2c1.Init.Timing = 0x30A0A7FB;
     hi2c1.Init.OwnAddress1 = 0;
     hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
     hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
     hi2c1.Init.OwnAddress2 = 0;
+    hi2c1.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
     hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
     hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
     if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+            {
+        Error_Handler();
+    }
+    /** Configure Analogue filter
+     */
+    if (HAL_I2CEx_ConfigAnalogFilter(&hi2c1, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
+            {
+        Error_Handler();
+    }
+    /** Configure Digital filter
+     */
+    if (HAL_I2CEx_ConfigDigitalFilter(&hi2c1, 0) != HAL_OK)
             {
         Error_Handler();
     }
@@ -193,35 +218,48 @@ static void MX_I2C1_Init(void)
 }
 
 /**
- * @brief USART2 Initialization Function
+ * @brief LPUART1 Initialization Function
  * @param None
  * @retval None
  */
-static void MX_USART2_UART_Init(void)
+static void MX_LPUART1_UART_Init(void)
 {
 
-    /* USER CODE BEGIN USART2_Init 0 */
+    /* USER CODE BEGIN LPUART1_Init 0 */
 
-    /* USER CODE END USART2_Init 0 */
+    /* USER CODE END LPUART1_Init 0 */
 
-    /* USER CODE BEGIN USART2_Init 1 */
+    /* USER CODE BEGIN LPUART1_Init 1 */
 
-    /* USER CODE END USART2_Init 1 */
-    huart2.Instance = USART2;
-    huart2.Init.BaudRate = 115200;
-    huart2.Init.WordLength = UART_WORDLENGTH_8B;
-    huart2.Init.StopBits = UART_STOPBITS_1;
-    huart2.Init.Parity = UART_PARITY_NONE;
-    huart2.Init.Mode = UART_MODE_TX_RX;
-    huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-    huart2.Init.OverSampling = UART_OVERSAMPLING_16;
-    if (HAL_UART_Init(&huart2) != HAL_OK)
+    /* USER CODE END LPUART1_Init 1 */
+    hlpuart1.Instance = LPUART1;
+    hlpuart1.Init.BaudRate = 115200;
+    hlpuart1.Init.WordLength = UART_WORDLENGTH_8B;
+    hlpuart1.Init.StopBits = UART_STOPBITS_1;
+    hlpuart1.Init.Parity = UART_PARITY_NONE;
+    hlpuart1.Init.Mode = UART_MODE_TX_RX;
+    hlpuart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+    hlpuart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+    hlpuart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+    if (HAL_UART_Init(&hlpuart1) != HAL_OK)
             {
         Error_Handler();
     }
-    /* USER CODE BEGIN USART2_Init 2 */
+    if (HAL_UARTEx_SetTxFifoThreshold(&hlpuart1, UART_TXFIFO_THRESHOLD_1_8) != HAL_OK)
+            {
+        Error_Handler();
+    }
+    if (HAL_UARTEx_SetRxFifoThreshold(&hlpuart1, UART_RXFIFO_THRESHOLD_1_8) != HAL_OK)
+            {
+        Error_Handler();
+    }
+    if (HAL_UARTEx_DisableFifoMode(&hlpuart1) != HAL_OK)
+            {
+        Error_Handler();
+    }
+    /* USER CODE BEGIN LPUART1_Init 2 */
 
-    /* USER CODE END USART2_Init 2 */
+    /* USER CODE END LPUART1_Init 2 */
 
 }
 
@@ -236,7 +274,7 @@ static void MX_GPIO_Init(void)
 
     /* GPIO Ports Clock Enable */
     __HAL_RCC_GPIOC_CLK_ENABLE();
-    __HAL_RCC_GPIOH_CLK_ENABLE();
+    __HAL_RCC_GPIOF_CLK_ENABLE();
     __HAL_RCC_GPIOA_CLK_ENABLE();
     __HAL_RCC_GPIOB_CLK_ENABLE();
 
@@ -276,18 +314,18 @@ void Error_Handler(void)
 
 #ifdef  USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t *file, uint32_t line)
 {
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
+    /* USER CODE BEGIN 6 */
+    /* User can add his own implementation to report the file name and line number,
      tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
+    /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
 
